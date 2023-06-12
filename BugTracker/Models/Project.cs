638e01c5734d161c2bcf3c8c7b2845ebbe8cee77
Michael <39811434+msawlani@users.noreply.github.com>
2023-06-12 17:19:59 -0400

    created project model
    created a model and context for projects so that they can be stored and created

---
+++ BugTracker/Models/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace BugTracker.Models
+{
+    public class Project
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Bug> Bugs { get; set; }
+    }
+}
---
+++ BugTracker/Models/ProjectContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Models
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<BugContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Project> project { get; set; }
+    }
+}
